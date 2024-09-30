6e1801341d5803ace96869e9f25859d575614f3a
Ryutaro Isobe <ryu@ryunoMacBook-Pro.local>
2024-09-30 21:08:11 +0900

    セキュアなパスワードを追加するため、Userモデルにpassword_digestを追記

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_09_29_214252) do
+ActiveRecord::Schema.define(version: 2024_09_30_112204) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
